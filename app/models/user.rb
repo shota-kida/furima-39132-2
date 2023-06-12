3e5b52690f997f799b5d1df3b84520309a3c7ac3
shota-kida <syooota775@gmail.com>
2023-06-12 12:50:36 +0900

    user.modelの設計

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+     
+         validates :email, presence: true, uniqueness: true
+         validates :password, presence: true, length: { minimum: 6 }
+         validates :name, presence: true
+         validates :nickname, presence: true
+         validates :name_zenkaku, presence: true
+         validates :name_kana, presence: true
+         validates :name_mei, presence: true
+         validates :birthday, presence: true
 end
