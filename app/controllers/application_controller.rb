fd7ddc16dc15e9210f65e742435b9ed6955ea597
shota-kida <syooota775@gmail.com>
2023-07-08 17:13:01 +0900

    最終課題（最終）修正❶

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'syota' && password == '0202'
+    end
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :nickname, :name_zenkaku, :name_kana, :name_mei, :birthday])
   end
